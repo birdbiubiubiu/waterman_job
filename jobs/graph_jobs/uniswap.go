a9e38b71a79e350967fdd1b354dde9fd854cc96a
szl <717540722z@qq.com>
2020-11-18 15:05:02 +0800

    modify

--- jobs/graph_jobs/uniswap.go
+++ jobs/graph_jobs/uniswap.go
@@ -1,6 +1,7 @@
 package graph_jobs
 
 import (
+	"time"
 	"waterman_job/models"
 	"waterman_job/pkg/e"
 	"waterman_job/pkg/logging"
@@ -13,15 +14,15 @@ type UniSwapGraphql struct {
 
 func (u UniSwapGraphql) Run() {
 	lastRecord, err := models.GetLastWhalesByAction(u.Action, graph_service.PlatformUniSwap)
-	lastTimestamp := int(0)
+	lastTimestamp := int(time.Now().Unix() - 86400)//默认一天前
 	query := ""
 	switch u.Action {
 	case "mint":
-		query = "query($allPairs: [Bytes]!,$lastTimestamp :BigInt!, $first :Int!){mints(first:$first, orderBy: timestamp, orderDirection: asc,where:{pair_in: $allPairs,timestamp_gt:$lastTimestamp,amountUSD_gt:100000}){pair{token0{symbol},token1{symbol}},amountUSD,id,amount0,amount1,timestamp,transaction{id,timestamp}}}"
+		query = "query($allPairs: [Bytes]!,$lastTimestamp :BigInt!, $first :Int!){mints(first:$first, orderBy: timestamp, orderDirection: asc,where:{pair_in: $allPairs,timestamp_gt:$lastTimestamp,amountUSD_gt:1000000}){pair{token0{symbol},token1{symbol}},amountUSD,id,amount0,amount1,timestamp,transaction{id,timestamp}}}"
 	case "burn":
-		query = "query($allPairs: [Bytes]!,$lastTimestamp :BigInt!, $first :Int!){burns(first:$first, orderBy: timestamp, orderDirection: asc,where:{pair_in: $allPairs,timestamp_gt:$lastTimestamp,amountUSD_gt:100000}){pair{token0{symbol},token1{symbol}},amountUSD,id,amount0,amount1,timestamp,transaction{id,timestamp}}}"
+		query = "query($allPairs: [Bytes]!,$lastTimestamp :BigInt!, $first :Int!){burns(first:$first, orderBy: timestamp, orderDirection: asc,where:{pair_in: $allPairs,timestamp_gt:$lastTimestamp,amountUSD_gt:1000000}){pair{token0{symbol},token1{symbol}},amountUSD,id,amount0,amount1,timestamp,transaction{id,timestamp}}}"
 	case "swap":
-		query = "query($allPairs: [Bytes]!,$lastTimestamp :BigInt!, $first :Int!){swaps(first:$first, orderBy: timestamp, orderDirection: asc,where:{pair_in: $allPairs,timestamp_gt:$lastTimestamp,amountUSD_gt:100000}){pair{token0{symbol},token1{symbol}},amountUSD,id,amount0,amount1,timestamp,transaction{id,timestamp}}}"
+		query = "query($allPairs: [Bytes]!,$lastTimestamp :BigInt!, $first :Int!){swaps(first:$first, orderBy: timestamp, orderDirection: asc,where:{pair_in: $allPairs,timestamp_gt:$lastTimestamp,amountUSD_gt:1000000}){pair{token0{symbol},token1{symbol}},amountUSD,id,amount0,amount1,timestamp,transaction{id,timestamp}}}"
 	}
 
 	if err != nil {
